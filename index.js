e04517548c044aa3745d034efa528b15c77ba04d
Joshua Tahanlangit <contact@thisjt.me>
2024-07-27 04:25:21 +0800

    add git worktrees support

--- index.js
+++ index.js
@@ -5,12 +5,18 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function getCurrentBranchName(p = process.cwd()) {
-  const gitHeadPath = `${p}/.git/HEAD`;
+module.exports = function getCurrentBranchName(p = process.cwd(), i = 0) {
+  if (i > 127) return false;
+  let gitHeadPath = '';
+  try {
+    gitHeadPath = `${fs.readFileSync(`${p}/.git`, 'utf-8').trim().replace('gitdir: ', '')}/HEAD`;
+  } catch {
+    gitHeadPath = `${p}/.git/HEAD`;
+  }
 
   return fs.existsSync(p) ?
       fs.existsSync(gitHeadPath) ?
           fs.readFileSync(gitHeadPath, 'utf-8').trim().split('/')[2] :
-          getCurrentBranchName(path.resolve(p, '..')) :
+          getCurrentBranchName(path.resolve(p, '..'), i++) :
       false
 };
